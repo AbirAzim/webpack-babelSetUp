c1fb039733dbda816deae7ab177484b07b15fbf7
bk <badhonkhanbk007@gmail.com>
2020-01-29 10:43:34 +0600

    babel and webpack set up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let obj = {
+    name: 'badhon',
+    age: 24,
+    profession: 'nothing'
+}
+
+let templateString = `name : ${obj.name}  age : ${obj.age} profession : ${obj.profession
+    }`
+
+console.log(templateString);
+console.log('live server is running on this');
